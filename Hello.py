25003f5e3f8a9dc92dd2ed9a61cd7065872c26a7
Karthika Venugopal K <41442849+prithvishrahul@users.noreply.github.com>
2023-07-29 20:31:40 +0530

    2nnd commit

--- Hello.py
+++ Hello.py
@@ -2,8 +2,33 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+Cities=[
+    {
+    'id':1,
+    'name':"India"
+    },
+    {
+    'id':2,
+    'name':"SriLanKa"
+    },
+    {
+    'id':1,
+    'name':"USA"
+    },
+    {
+    'id':1,
+    'name':"China"
+    }
+]
+
 @app.route("/")
 def hello_rahul():
-    return render_template('Index.html')
+    print(takewebcode())
+    return render_template('Index.html',cities=Cities)
 
+
+def takewebcode():
+     s=render_template('Index.html',cities=Cities)
+     return s
+    
 app.run(host="0.0.0.0",debug=True)
